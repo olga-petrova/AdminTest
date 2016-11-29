59db022d743dd115f9d3877662d83d6ccf1c63cb
Olga Petrova <Olga@Olgas-MacBook-Pro-2.local>
2016-11-29 08:39:45 +0100

    Webdriver update

--- test/uiunit/view/dashboard/todowindow/TodoWindow.js
+++ test/uiunit/view/dashboard/todowindow/TodoWindow.js
@@ -31,7 +31,7 @@ describe("TODO window UI unit test suite", function() {
         });
     });
     
-    xit("ToDo window should match the expected screenshot", function(done) {
+    it("ToDo window should match the expected screenshot", function(done) {
         ST.screenshot('todowindow', done);
     }, 30000);
 
